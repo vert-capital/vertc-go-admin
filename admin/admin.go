9aa1dc9830ff191ab3a8042f81fe782f31434433
Caio Faria <caio.faria@vert-capital.com>
2024-06-21 16:23:26 -0300

    create package admin

---
+++ admin/admin.go
@@ -0,0 +1,27 @@
+package admin
+
+import (
+	"github.com/vert-capital/vertc-go-admin/api"
+	"github.com/vert-capital/vertc-go-admin/entity"
+	postgres "github.com/vert-capital/vertc-go-admin/infrasctructure/database"
+	"github.com/vert-capital/vertc-go-admin/infrasctructure/kafka"
+	"gorm.io/gorm"
+)
+
+var Tables map[string]entity.Table
+
+var DB *gorm.DB
+
+func AddTable(table entity.Table) {
+	Tables[table.Name] = table
+}
+
+func SetDB(db *gorm.DB) {
+	DB = db
+}
+
+func RunServer(db *gorm.DB, port string) {
+	postgres.Migrations(db)
+	go kafka.StartKafka(db)
+	api.StartWebServer(db, port)
+}
--- main.go
+++ main.go
@@ -1,30 +1,4 @@
 package main
 
-import (
-	"github.com/vert-capital/vertc-go-admin/api"
-	"github.com/vert-capital/vertc-go-admin/entity"
-	postgres "github.com/vert-capital/vertc-go-admin/infrasctructure/database"
-	"github.com/vert-capital/vertc-go-admin/infrasctructure/kafka"
-	"gorm.io/gorm"
-)
-
-var Tables map[string]entity.Table
-
-var DB *gorm.DB
-
-func AddTable(table entity.Table) {
-	Tables[table.Name] = table
-}
-
-func SetDB(db *gorm.DB) {
-	DB = db
-}
-
 func main() {
-}
-
-func RunServer(db *gorm.DB, port string) {
-	postgres.Migrations(db)
-	go kafka.StartKafka(db)
-	api.StartWebServer(db, port)
 }
