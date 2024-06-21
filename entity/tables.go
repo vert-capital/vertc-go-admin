eee1289f7bce328985a4252e6fbb30699ce22568
Caio Faria <caio.faria@vert-capital.com>
2024-06-21 16:30:20 -0300

    fix tables

--- entity/tables.go
+++ entity/tables.go
@@ -5,9 +5,9 @@ type Fields map[string]interface{}
 type Actions map[string](func() error)
 
 type Table struct {
-	Name         string    `json:"name"`
-	Category     string    `json:"category"`
-	Fields       Fields    `json:"fields"`
-	SearchFields []*string `json:"search_fields"`
-	Actions      Actions   `json:"actions"`
+	Name         string   `json:"name"`
+	Category     string   `json:"category"`
+	Fields       Fields   `json:"fields"`
+	SearchFields []string `json:"search_fields"`
+	Actions      Actions  `json:"actions"`
 }
