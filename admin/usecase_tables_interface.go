02bca62777ee386f595a3cc3677ae8afc9e12ed2
Caio Faria <caio.faria@vert-capital.com>
2024-06-24 10:16:33 -0300

    fix usecases

--- admin/usecase_tables_interface.go
+++ admin/usecase_tables_interface.go
@@ -2,7 +2,7 @@ package vertc_go_admin
 
 type IRepositoryTable interface {
 	List(table Table, filters Filters) (response ResponseList, err error)
-	Get(table Table, id int) (response []map[string]interface{}, err error)
+	Get(table Table, id int) (response map[string]interface{}, err error)
 	Create(table Table, fields Fields) (response ResponseCreateUpdateDelete, err error)
 	Update(table Table, fields Fields, id int) (response ResponseCreateUpdateDelete, err error)
 	Delete(table Table, ids []int) (response ResponseCreateUpdateDelete, err error)
@@ -10,7 +10,7 @@ type IRepositoryTable interface {
 
 type IUseCaseTable interface {
 	List(table Table, filters Filters) (response ResponseList, err error)
-	Get(table Table, id int) (response []map[string]interface{}, err error)
+	Get(table Table, id int) (response map[string]interface{}, err error)
 	Create(table Table, fields Fields) (response ResponseCreateUpdateDelete, err error)
 	Update(table Table, fields Fields, id int) (response ResponseCreateUpdateDelete, err error)
 	Delete(table Table, ids []int) (response ResponseCreateUpdateDelete, err error)
