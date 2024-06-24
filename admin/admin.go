b29529de64e74fd34fd8be7560af25a933618d97
Caio Faria <caio.faria@vert-capital.com>
2024-06-24 09:51:59 -0300

    create getfields function

--- admin/admin.go
+++ admin/admin.go
@@ -1,10 +1,39 @@
 package vertc_go_admin
 
 import (
+	"reflect"
+
 	"gorm.io/gorm"
 )
 
 var Tabelas Tables
+
+func GetFields(structModel struct{}) *Fields {
+	campos := make(Fields)
+	example := structModel
+	v := reflect.ValueOf(example)
+	typeOfTable := v.Type()
+
+	for i := 0; i < v.NumField(); i++ {
+		fieldName := typeOfTable.Field(i).Name
+
+		// Determinando o tipo do campo
+		fieldType := typeOfTable.Field(i).Type.Kind().String()
+
+		// Estrutura a ser armazenada no mapa
+		if fieldType == "ptr" {
+			fieldType = "datetime"
+		}
+		fieldInfo := map[string]interface{}{
+			"type":       fieldType,
+			"required":   true,
+			"json_field": typeOfTable.Field(i).Tag.Get("json"),
+		}
+
+		campos[fieldName] = fieldInfo
+	}
+	return &campos
+}
 
 func AddTabela(tabela Table) {
 	if Tabelas == nil {
